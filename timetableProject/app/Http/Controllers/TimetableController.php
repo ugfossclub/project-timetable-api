479faa8cd71cea7fa65a1db607c1f97d3f246cb0
totality_taekonda <ctriston34@gmail.com>
2020-02-15 17:12:09 -0400

    add table to model

--- timetableProject/app/Http/Controllers/TimetableController.php
+++ timetableProject/app/Http/Controllers/TimetableController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Timetable;
 
 class TimetableController extends Controller
 {
-    //
+
+
+    public function index(Timetable $timetable)
+    {
+
+        $timetable=App\Timetable::all();
+        dd($timetable);
+    }
+
+    public function create()
+    {
+
+    }
+
+
+    public function store()
+    {
+
+        $request = new Timetable();
+        $request->room=request('');
+        $request->course = request('');
+        $request->day = request('');
+        $request->time= request('');
+        $request->save()
+        
+
+
+    }
+    public function update()
+    {
+
+    }
+    public function delete()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
 }
