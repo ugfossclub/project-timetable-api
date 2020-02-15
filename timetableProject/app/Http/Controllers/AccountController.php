0fdc2130e8c03ea91675b77456b17b4e29947edc
griffith2806 <danielbgriffith2806@gmail.com>
2020-02-15 15:39:03 -0400

    *Initial commit

---
+++ timetableProject/app/Http/Controllers/AccountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\account;
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function show(account $account)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(account $account)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, account $account)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\account  $account
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(account $account)
+    {
+        //
+    }
+}
---
+++ timetableProject/database/migrations/2020_02_15_185240_create_accounts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('ugId');
+            $table->string('type');
+            $table->string('fName');
+            $table->string('lName');
+            $table->string('faculty');
+            $table->string('password');
+            $table->string('department');
+            $table->string('programme');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
