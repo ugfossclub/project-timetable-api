d9c044144e5a272dc40543b09a78cb5873b67bbf
AndrewGY <37263878+AndrewGY@users.noreply.github.com>
2020-02-15 15:46:20 -0400

    Add files via upload

---
+++ timetableProject/database/migrations/2020_02_15_192908_create_timetables_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimetablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetables', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('room');
+            $table->string('course');
+            $table->string('day');
+            $table->time('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetables');
+    }
+}
