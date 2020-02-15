a87183623760673132cfc524052560e86cb9b880
griffith2806 <danielbgriffith2806@gmail.com>
2020-02-15 17:26:52 -0400

    Routes added & Controllers updated

--- timetableProject/routes/api.php
+++ timetableProject/routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('account', 'AccountController@index');
+Route::get('account/{id}', 'AccountController@show');
+Route::post('account', 'AccountController@store');
+Route::put('account/{id}', 'AccountController@update');
+Route::delete('account/{id}', 'AccountController@delete');
+Route::get('timetable', 'TimetableController@index');
+Route::get('timetable/{id}', 'TimetableController@show');
+Route::post('timetable', 'TimetableController@store');
+Route::put('timetable/{id}', 'TimetableController@update');
+Route::delete('timetable/{id}', 'TimetableController@delete');
